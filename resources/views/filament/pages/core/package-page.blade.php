cc8d822348ee558ea6528614f5effb5154a9a167
Nashwan Dlshad <98420403+Nashwan-Dlshad@users.noreply.github.com>
2024-06-22 17:30:57 +0300

    last commit

--- resources/views/filament/pages/core/package-page.blade.php
+++ resources/views/filament/pages/core/package-page.blade.php
@@ -16,7 +16,7 @@
     @endforeach
 
 </div>
-    <div class="grid grid-cols-1 gap-5 xl:grid-cols-2 ctn fi-section text-primary-500">
+    <div class="grid grid-cols-1 gap-5 xl:grid-cols-2 ctn fi-section">
         <x-filament::section>
             <form wire:submit='uploadPackage'>
                 {{ $this->uploadPackageForm }}
@@ -28,34 +28,6 @@
         </x-filament::section>
         <x-filament::section>
            {{ $this->table }}
-        </x-filament::section>
-
-    </div>
-</x-filament-panels::page>
-
-<x-filament-panels::page>
-
-<div class="grid grid-cols-4">
-    <livewire:core.package-overview
-    name="First Package"
-    version="2.1"
-    description="The first package description for the application "
-    active_date="{{ now()->format('Y-m-d') }}"
-    type="Primary"
-    status="false"
-    color="#088395"
-    price="200"
-     />
-</div>
-    <div class="grid grid-cols-2 ctn fi-section">
-        <x-filament::section>
-            <form wire:submit='uploadPackage'>
-                {{ $this->packageForm }}
-
-                <x-filament::button type="submit" class="mt-5">
-                    {{ trans('filament-actions::modal.actions.submit.label') }}
-                </x-filament::button>
-            </form>
         </x-filament::section>
 
     </div>
