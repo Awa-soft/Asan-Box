81f9daaed26efb770aba8ce2a969e26e7cef6479
Nashwan Dlshad <98420403+Nashwan-Dlshad@users.noreply.github.com>
2024-06-22 17:40:13 +0300

    last commit with package page

---
+++ lang/en/POS/lang.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    "group_label"=>"POS",
+    'purchase'=>[
+        'plural_label'=>'Purchases',
+        'singular_label'=>'Purchase'
+    ],
+    'purchase_return'=>[
+        'plural_label'=>'Purchase Returns',
+        'singular_label'=>'Purchase Return'
+    ],
+];
